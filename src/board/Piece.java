5c6b66647ead8d611e30655c70420ca0e98a6fdc
larybino <binolaryssa@gmail.com>
2024-06-18 11:23:08 -0300

    modificando Piece

--- src/board/Piece.java
+++ src/board/Piece.java
@@ -3,7 +3,7 @@ public abstract class Piece {
     protected Position position;
     private Board board;
 
-    public Piece(Position position, Board board) {
+    public Piece(Board board) {
         this.board = board;
         position = null;
     }
